92c26bb5303e9cbc895e58e0f10be5bae51390eb
Sergio <illescas.s.m@gmail.com>
2022-09-20 19:03:26 -0300

    probando

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+    static: "./build"
+});
 server.use(middlewares);
+server.use(
+    jsonServer.rewriter()
+)
 server.use(router);
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
